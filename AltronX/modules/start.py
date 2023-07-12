517a730c3e50bb1c190392ce8c56dfc3b8b1d06a
itszshivam <139079474+itszshivam@users.noreply.github.com>
2023-07-12 18:43:42 +0530

    start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -8,11 +8,11 @@ PythonButton = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
         ],
         [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/ABOUT_GODFATHER"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/INCRICIBLE")
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/itsz_shivam"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/itsz_shivam")
         ],
         [
-        Button.url("• ʀᴇᴘᴏ •", "https://t.me/SANA_SUPPORTS")
+        Button.url("• ʀᴇᴘᴏ •", "https://t.me/itsz_shivam")
         ]
         ]
 
@@ -34,11 +34,11 @@ async def start(event):
         BotName = AltBot.first_name
         BotId = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝙶𝙾𝙳𝙵𝙰𝚃𝙷𝙴𝚁](https://t.me/YOUR_GODFATHER_XD)**\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [ʂɧı۷ąɱ](https://t.me/itsz_shivam)**\n\n"
         TEXT += f"» **ʙᴏᴛ ꜱᴘᴀᴍ ᴠᴇʀsɪᴏɴ :** `M3.2`\n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                 event.chat_id,
-                "https://te.legra.ph/file/1850db82b9ae8dd870a8f.jpg",
+                "https://telegra.ph/file/0de98f5fe87df8e6b514d.jpg",
                 caption=TEXT, 
                 buttons=PythonButton)

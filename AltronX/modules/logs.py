163b2cf40036eacd05360022d798a8fdc582070b
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-14 10:06:21 +0530

    Update logs.py

--- AltronX/modules/logs.py
+++ AltronX/modules/logs.py
@@ -39,8 +39,8 @@ async def logs(legend):
         await asyncio.sleep(1)
         await fetch.delete()
         logfile = open("skspam.txt", "w")
-        logfile.write("⚡ OPshivamX ⚡ [ BotSpam Logs ]\n\n" + logs)
+        logfile.write("⚡ 𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑 ⚡ [ BotSpam Logs ]\n\n" + logs)
         logfile.close()
-        await MK1.send_file(legend.chat_id, "skspam.txt", caption=f"⚡ ​ᴏᴘꜱʜɪᴠᴀᴍ ⚡\n  » **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
+        await MK1.send_file(legend.chat_id, "skspam.txt", caption=f"⚡ 𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑 ⚡\n  » **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
     else:
         await legend.reply("» ꜱᴏʀʀʏ, ᴏɴʟʏ ᴏᴡɴᴇʀ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ.")

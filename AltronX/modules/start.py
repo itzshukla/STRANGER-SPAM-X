99185315212432a12ff53aee8d283acefb394b79
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-14 10:01:55 +0530

    Update start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -8,11 +8,11 @@ PythonButton = [
         Button.inline("☆ 𝐂σммαиԃ𝐒 ☆", data="help_back")
         ],
         [
-        Button.url("☆ 𝐂нαииє𝐋 ☆", "https://t.me/over_poweredd"),
-        Button.url("☆ 𝐒υρρσя𝐓", "https://t.me/fun_here")
+        Button.url("☆ 𝐂нαииє𝐋 ☆", "https://t.me/mastiwithfriendsx"),
+        Button.url("☆ 𝐒υρρσя𝐓", "https://t.me/mastiwithfriendsx")
         ],
         [
-        Button.url("☆ 𝐑ꫀρ𝐎", "https://t.me/fun_here")
+        Button.url("☆ 𝐑ꫀρ𝐎", "https://t.me/mastiwithfriendsx")
         ]
         ]
 
@@ -34,11 +34,11 @@ async def start(event):
         BotName = AltBot.first_name
         BotId = AltBot.id
         TEXT = f"**𝐇𝐞𝐲 [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝐈 𝐀𝐦 [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **✦ 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐃 𝐁𝐘 :~ [𝐒ᴘᴇᴄᴛʀᴇ](https://t.me/itsz_shivam)**\n\n"
-        TEXT += f"» **𝐎𝐏 𝐒𝐏𝐀𝐌 𝐕𝐄𝐑𝐒𝐈𝐎𝐍 :** `3.2`\n"
+        TEXT += f"» **✦ 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐃 𝐁𝐘 :~ [𝐒ᴘᴇᴄᴛʀᴇ](https://t.me/shiva_ansh_op)**\n\n"
+        TEXT += f"» **𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑 𝐒𝐏𝐀𝐌 𝐕𝐄𝐑𝐒𝐈𝐎𝐍 :** `3.2`\n"
         TEXT += f"» **𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍:** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                 event.chat_id,
-                "https://telegra.ph/file/2b47da71aa223350b3076.jpg",
+                "https://telegra.ph/file/aa4bf1e57d11fb75b602e.jpg",
                 caption=TEXT, 
                 buttons=PythonButton)
